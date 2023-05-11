576a9855f2c26d8645ef77a883c2d728ab482ec6
Milan Shrestha <shrestham780@gmail.com>
2023-05-11 07:23:55 +0545

    writing comment

--- script.js
+++ script.js
@@ -4,19 +4,20 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+// for displaying message after user provides input
 const displayMessage = function (information) {
   document.querySelector(".message").textContent = information;
 };
 
+// check the numbers
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess, typeof guess);
 
   // When there is no input
   if (!guess) {
     displayMessage("⛔️ No number!");
 
-    // When player wins
+    // When user guess is right
   } else if (guess === secretNumber) {
     displayMessage("🎉 Correct Number!");
     document.querySelector(".number").textContent = secretNumber;
@@ -29,7 +30,7 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".highscore").textContent = highScore;
     }
 
-    // When guess is wrong
+    // When user guess is wrong
   } else if (guess !== secretNumber) {
     if (score > 1) {
       displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
@@ -42,6 +43,7 @@ document.querySelector(".check").addEventListener("click", function () {
   }
 });
 
+// resetting the game
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
